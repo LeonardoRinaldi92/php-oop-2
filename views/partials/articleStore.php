170c1d6c13ee36ee2d615b3829786c2f1caf0a0b
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-06-04 11:20:45 +0200

    aggiunto campo nullo

--- views/partials/articleStore.php
+++ views/partials/articleStore.php
@@ -11,30 +11,28 @@ include_once __DIR__ . '/../../components/functions.php'
 
 ?>
 <form action="index.php" method="POST">
-    <div class="row">
-        <div class="col-2">
-            <select class="form-select" aria-label="animale" name="animale">
-                <option selected>Che animale cerchi?</option>
-                <option value="1">cane</option>
-                <option value="2">gatto</option>
-            </select>
-        </div>
-        <div class="col-3">
-            <select class="form-select" aria-label="genere" name="genere">
-                <option selected value="">Che genere cerchi?</option>
-                <option value="cibo">Cibo</option>
-                <option value="cura">articoli per la cura del tuo animale</option>
-                <option value="accessori">accessori per la cura del tuo animale</option>
-            </select>
-        </div>
-    
-        <div class="col-1">
-        <button type="submit">
-            CERCA
-    
-        </button>
-        
-
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-2">
+                <select class="form-select" aria-label="animale" name="animale">
+                    <option selected>Che animale cerchi?</option>
+                    <option value="1">cane</option>
+                    <option value="2">gatto</option>
+                </select>
+            </div>
+            <div class="col-3">
+                <select class="form-select" aria-label="genere" name="genere">
+                    <option selected value="">Che genere cerchi?</option>
+                    <option value="cibo">Cibo</option>
+                    <option value="cura">articoli per la cura del tuo animale</option>
+                    <option value="accessori">accessori per il tuo animale</option>
+                </select>
+            </div> 
+            <div class="col-1">
+                <button type="submit">
+                    CERCA
+                </button>
+            </div>
         </div>
     </div>
 </form>
@@ -73,4 +71,13 @@ include_once __DIR__ . '/../../components/functions.php'
             </div>
         <?php } ?>
     </div>
+    <?php if(empty($filteredArray)) : ?>
+        <div class="text-center">
+            <p>
+                <i>
+                    Nessun Risultato
+                </i>
+            </p>
+        </div> 
+    <?php endif; ?>
 </div>
